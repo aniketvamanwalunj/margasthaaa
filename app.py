9bc6afa6815958e8a51ce1b5efdcc4888cdaaee5
Aniket Walunj <142065953+aniketvamanwalunj@users.noreply.github.com>
2025-09-10 23:11:18 +0530

    app.py

--- app.py
+++ app.py
@@ -2,10 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Route for home page
 @app.route("/")
 def home():
-    return render_template("margasthaaa.html")   # make sure your HTML file is named index.html
+    return render_template("margasthaaa.html")   # index.html should be inside templates folder 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5001)  
